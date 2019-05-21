74ccec86ab52d29b099f985fa1d8bdafa659df23
isa <muhammedisa54@gmail.com>
2019-05-21 16:00:14 +0300

    Adding the token authentication modules.

---
+++ WebServisInterface/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebServisInterface.Controllers
+{
+    //şifremeleme işlemi yapıp herkese erişimi engelleniyor.
+    [Authorize] 
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using(EmployeeDbEntities entities = new EmployeeDbEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
